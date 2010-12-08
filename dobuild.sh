5af739ee8e3b079615c61b1a2f0de1bbc359ec1a
Koert van der Veer <koert@openpanel.com>
2010-12-08 13:22:01 +0000

    Simple build&upload script

---
+++ dobuild.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Make a temporary file and try to sign it, so the gpg passphrase lives in the gpg-agent
+TMPFILE=`tempfile`
+gpg --clearsign ${TMPFILE}
+rm -f "${TMPFILE}"
+rm -f "${TMPFILE}.asc"
+rm -f "${TMPFILE}.sig"
+
+./autobuilder
+
+rsync -av /root/repository/* root@bob.openpanel.com:/srv/openpanel_repository --exclude "db/" --exclude "conf/"
