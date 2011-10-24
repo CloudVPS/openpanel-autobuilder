ff8d341f48542d4022f6cb4a65aebbc660f1a272
Koert van der Veer <koert@openpanel.com>
2011-10-24 10:13:53 +0200

    Restructure apt repo to avoid warnings on apt-get update.

--- dobuild.sh
+++ dobuild.sh
@@ -1,14 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+function repo_alias {
+    local from=$1
+    local to=$2
+    
+    reprepro -b "/root/repository" -A "source" listfilter $1 'Section (!=1)' | cut -d' ' -f2 | sort -u > "/tmp/dobuild.$$"
+    
+    while read pkg ; do
+        #echo $pkg
+        reprepro -b "/root/repository" copysrc "$to" "$from" "$pkg" 2> /dev/null 1>/dev/null
+    done <  "/tmp/dobuild.$$"
+    
+    rm -f "/tmp/dobuild.$$"
+} 
 
 # Make a temporary file and try to sign it, so the gpg passphrase lives in the gpg-agent
 TMPFILE=`tempfile`
 gpg --clearsign ${TMPFILE}
 rm -f "${TMPFILE}" "${TMPFILE}.asc" "${TMPFILE}.sig"
 
-./autobuilder --distribution=lenny   all
-./autobuilder --distribution=squeeze all
+./autobuilder --distribution=lenny  all
+./autobuilder --distribution=squeeze core modules openapp
+repo_alias squeeze wheezy
+
 ./autobuilder --distribution=lucid   all
-./autobuilder --distribution=natty   all
+repo_alias lucid    maverick
+
+./autobuilder --distribution=natty   core modules openapp
+repo_alias natty    oneiric
+repo_alias oneiric  precise
 
 for pkg in `rsync -av /root/repository/* root@bob.openpanel.com:/srv/openpanel_repository --exclude "db/" --exclude "conf/" | 
     grep -oe '^pool/.*\.deb' |
@@ -18,7 +38,7 @@ for pkg in `rsync -av /root/repository/* root@bob.openpanel.com:/srv/openpanel_r
     echo -n "OpenPanel Builder just uploaded $pkg" | nc -uq0 krakras.office.xl-is.net 18000
 done
 
-./autobuilder --force-tip
+./autobuilder --distribution=lenny --force-tip
 rsync -av /root/repository/* 'root@bob.openpanel.com:/srv/openpanel_repository' --exclude "db/" --exclude "conf/" |
     grep -oe '^pool/.*\.deb' | 
     grep "+tip" | 
@@ -26,5 +46,4 @@ rsync -av /root/repository/* 'root@bob.openpanel.com:/srv/openpanel_repository' 
     sort -u |
     xargs -rs 200 echo "New TIP for: " | 
     nc -uq0 krakras.office.xl-is.net 18000
-
 
